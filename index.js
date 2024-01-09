93a0e36083218d9ccc0b1258ca2aba36475d76ca
Himanshu <himanshuagl18@gmail.com>
2024-01-10 03:27:47 +0530

    creating https server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const check = 10;
+const express =require("express");
+const app=express();
+
+const bodyparser=require("body-parser");
+app.use(bodyparser.json())
+
+const port=process.env.PORT || 3000;
+app.get("/first",(req,res)=>{
+    res.json({
+        name:"himanshu",
+        age:"21"
+    })
+})
+app.post("/post",(req,res)=>{
+    console.log(req.body);
+    res.send("its a post request")
+})
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+app.listen(port,()=>{
+    console.log(`on port ${port}`);
+})
